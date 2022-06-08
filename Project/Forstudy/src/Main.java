deef1e8777077b08106156a754f2f192d4491c0d
HalfMoon1008 <yunsh1008@naver.com>
2022-06-08 17:08:36 +0900

    selfstudy
    git reindexing

---
+++ Project/Forstudy/src/Main.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Cmd + shift + F11
+		
+		// Q1. System.out.println("Hello World!");
+		
+		// Q2. System.out.println("강한친구 대한육군\n강한친구 대한육군\n");
+		
+	    // System.out.println("강한친구 대한육군");
+	    // System.out.println("강한친구 대한육군\n");
+	    
+	    
+//	    System.out.println("강한친구 대한육군");
+//	    System.out.print("강한친구 대한육군\n");	    
+	    
+	    // 엔터키 역할(한 줄 띄기) - println or \n
+		
+//		Q3.
+//		System.out.println("\\    /\\");
+//		System.out.println(" )  ( ')");
+//		System.out.println("(  /  )");
+//		System.out.println(" \\(__)|");
+		
+	    
+//	    Q4.
+//		System.out.println("|\\_/|");
+//		System.out.println("|q p|   /}");
+//		System.out.println("( 0 )\"\"\"\\");
+//		System.out.println("|\"^\"`    |");
+//		System.out.println("||_/=\\\\__|");
+
+//		Q5.
+//		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		System.out.println(a+b);
+		
+		
+		
+//		Q6
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
--- Project/algorithm/src/algorithm/Main.java
+++ Project/algorithm/src/algorithm/Main.java
@@ -1,36 +1,11 @@
 package algorithm;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		System.out.println("입력해주세요 : ");
-		String text = br.readLine();
-		String[] results = new String[text.length()];
-
-		for (int i = 0; i < text.length(); i++) {
-			results[i] = text.substring(i, text.length());
-		}
-
-		Arrays.sort(results); // 오름차순으로 배열 정렬
-
-		for (int j = 0; j < results.length; j++) {
-			bw.write(results[j]);
-			bw.newLine();
-		}
-
-		bw.flush();
-		bw.close();
 	}
 
 }
